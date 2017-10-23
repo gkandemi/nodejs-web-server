683b9aaa9f2f73ba173c6726f531730fbddab316
gkandemi <gokhan@gkandemir.com>
2017-10-23 22:41:57 +0300

    Heroku için PORT güncellendi.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var middleware = require("./middleware");
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(middleware.logger);
 
