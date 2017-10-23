16d7f64e2b26b46c010bbd56ea60a6337b0338aa
gkandemi <gokhan@gkandemir.com>
2017-10-23 21:46:03 +0300

    Middleware modül olarak ayrildi.

---
+++ middleware.js
@@ -0,0 +1,12 @@
+var middleware = {
+    requireAuthentication: function (req, res, next) {
+        console.log("Özel route girildi!!!");
+        next();
+    },
+    logger: function (req, res, next) {
+        console.log(req.method + " " + req.originalUrl);
+        next();
+    }
+}
+
+module.exports = middleware;
--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 var express = require("express");
+var middleware = require("./middleware");
 var app = express();
 var PORT = 3000;
-
-var middleware = {
-    requireAuthentication: function (req, res, next) {
-        console.log("Özel route girildi!!!");
-        next();
-    },
-    logger: function (req, res, next) {
-        console.log(req.method + " " + req.originalUrl);
-        next();
-    }
-}
 
 app.use(middleware.logger);
 
