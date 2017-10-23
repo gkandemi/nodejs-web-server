ca9a0fa034982dc9f406a73be949f5cb1a157a11
gkandemi <gokhan@gkandemir.com>
2017-10-23 21:15:39 +0300

    repository init edildi

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+    requireAuthentication: function (req, res, next) {
+        console.log("Özel route girildi!!!");
+        next();
+    },
+    logger: function (req, res, next) {
+        console.log(req.method + " " + req.originalUrl);
+        next();
+    }
+}
+
+app.use(middleware.logger);
+
+app.get("/hakkimda", middleware.requireAuthentication, function (req, res) {
+    res.send("Hakkımda sayfası!!!");
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+    console.log("Express server " + PORT + " nolu portta çalışıyor...");
+});
